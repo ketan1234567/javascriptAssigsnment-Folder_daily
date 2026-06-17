7e1248e6a615fc5361cc1d4c24d36fcc096c4cd5
ketanDeshmukh <deshmukhketan647@gmail.com>
2026-06-18 03:58:40 +0530

    "Today_task_done"

---
+++ June-Node/Day-15/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const userRoutes=require('./routes/userRoutes')
+const app = express();
+
+app.use(express.json());
+
+
+app.use("/api",userRoutes)
+
+app.listen(3000, () => {
+  console.log("Server Started");
+});
